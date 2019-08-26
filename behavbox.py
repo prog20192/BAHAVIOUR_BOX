1b98f7c0fdd86b9679ef068b6a48a2ff6cf708a7
tassianunes <54067496+tassianunes@users.noreply.github.com>
2019-08-26 11:43:06 -0300

    Update behavbox.py

--- behavbox.py
+++ behavbox.py
@@ -5,3 +5,14 @@ print('''Suas opções:
 [ 0 ] Não habituado
 [ 1 ] Habituação\n''')
 habituacao = int(input('O animal está habituado: '))
+if habituacao:
+    print('\nAlguns parâmetros serão avaliados na seguinte ordem!')
+    print('ETAPA 1: Aproximação da barra')
+    print('ETAPA 2: Tocar na barra')
+    print('ETAPA: Discriminar o som')
+    print('\nETAPA 1: APROXIMAÇÃO')
+    distancia = int(input('Distância do animal até a barra: '))
+    if distancia <= 30:
+        print('Foi liberado 0,5 ml de recompensa!')
+        print('O animal passou para etapa 2')
+        print('\nETAPA 2: TOCAR NA BARRA')
